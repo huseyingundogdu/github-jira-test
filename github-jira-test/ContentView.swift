af59deb82ba36e932d27be571704093a396f93cc
Huseyin <benhuseyingundogdu@gmail.com>
2024-11-14 17:10:12 +0100

    TG-1 jira connection test 1

--- github-jira-test/ContentView.swift
+++ github-jira-test/ContentView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        //Test1
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
