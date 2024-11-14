73a86c7fbb708150793b25f6cd6624f7187e8f63
Huseyin <benhuseyingundogdu@gmail.com>
2024-11-14 17:07:12 +0100

    Initial Commit

---
+++ github-jira-test/github_jira_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_jira_testApp.swift
+//  github-jira-test
+//
+//  Created by Huseyin on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct github_jira_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
